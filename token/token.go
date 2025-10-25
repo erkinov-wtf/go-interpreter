9141d6f8e11b2acde2e15f99e0dedd561119595f
Rakhmatilla Erkinov <erkinovit@gmail.com>
2025-10-25 16:44:37 +0500

    feat(token): Add token definition
    - Define TokenType and Token struct
    - Add constants for token types

---
+++ token/token.go
@@ -0,0 +1,30 @@
+package token
+
+type TokenType string //TODO converting it to int or byte for perf
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
